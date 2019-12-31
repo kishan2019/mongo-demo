8bcfbc895bef09a3c0fac4721cba760c84579cc2
kishan singh <kishan.singh@mountblue.io>
2019-12-31 23:34:24 +0530

    model created.

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, use
     .then(() => console.log('Connected to mongodb..'))
     .catch( e => console.log('Could not connected to mongodb ', e));
 
+//Created Schema 
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
-    tag: [String],
+    tag: [ String ],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 });
+
+//created model
+ const Course = mongoose.model('Course', courseSchema);
+ const course = new Course ({
+     name: 'NODE.J course',
+     author: 'Kishan',
+     tag: ['front', 'backend'],
+     isPublished: true
+ });
+
+ 
